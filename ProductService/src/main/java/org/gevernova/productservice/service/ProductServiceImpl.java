7b727c7eed18f5cae0378590903a83ccd1708846
XEROX112 <jayabhardwaz1@gmail.com>
2026-02-19 20:29:22 +0530

    [Aryan] :[ProductService: Add Products Implementation with CRUD]

---
+++ ProductService/src/main/java/org/gevernova/productservice/service/ProductServiceImpl.java
@@ -0,0 +1,42 @@
+package org.gevernova.productservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.gevernova.productservice.entity.Product;
+import org.gevernova.productservice.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService{
+    private final ProductRepository repository;
+
+    public Product addProduct(Product product) {
+        return repository.save(product);
+    }
+
+    public List<Product> getAll() {
+        return repository.findAll();
+    }
+
+    public Product getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+    }
+
+    public void reduceQuantity(Long id, int quantity) {
+        Product product = getById(id);
+
+        if (product.getQuantity() < quantity) {
+            throw new InsufficientStockException("Insufficient stock");
+        }
+
+        product.setQuantity(product.getQuantity() - quantity);
+        repository.save(product);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
