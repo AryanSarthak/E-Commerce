fd40d46b92c0a1e003271c71369c98e0bbcaa7fe
XEROX112 <jayabhardwaz1@gmail.com>
2026-02-19 21:00:35 +0530

    [Aryan] :[OrderService :  Create JPA Repository for Orders]

---
+++ OrderService/src/main/java/org/gevernova/orderservice/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package org.gevernova.orderservice.repository;
+
+
+import org.gevernova.orderservice.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
