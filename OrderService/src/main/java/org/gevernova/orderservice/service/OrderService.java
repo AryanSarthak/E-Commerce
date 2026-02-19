14be01aa038afbe8497ec06aea935a352355b0a7
XEROX112 <jayabhardwaz1@gmail.com>
2026-02-19 21:17:14 +0530

    [Aryan] :[OrderService : Update OrderService Layer]

--- OrderService/src/main/java/org/gevernova/orderservice/service/OrderService.java
+++ OrderService/src/main/java/org/gevernova/orderservice/service/OrderService.java
@@ -6,5 +6,8 @@ public interface OrderService {
 
     // Place a new order for a product with given quantity
     Order place(Long productId, int quantity);
+
+    // Retrieve order details by order ID
+    Order getById(Long id);
 }
 
