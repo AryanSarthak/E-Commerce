ab273b215c7224d5fc7eb15da7283e06f7089f10
XEROX112 <jayabhardwaz1@gmail.com>
2026-02-19 21:47:20 +0530

    [Aryan] :[OrderService :Update FeginClient]

--- OrderService/src/main/java/org/gevernova/orderservice/external/service/ProductClient.java
+++ OrderService/src/main/java/org/gevernova/orderservice/external/service/ProductClient.java
@@ -4,7 +4,7 @@ import org.gevernova.orderservice.dto.ProductResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "PRODUCT-SERVICE")
+@FeignClient(name = "PRODUCTSERVICE")
 public interface ProductClient {
 
     // Fetch product details by product ID
